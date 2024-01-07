360c950d7ec170e05779bd9ab15a87117ea01720
Daniel Liegl <d.liegl@student.tugraz.at>
2024-01-07 11:33:48 +0100

    added delete function

---
+++ client/netlify/functions/delete_script/delete_script.js
@@ -0,0 +1,47 @@
+const { connectToDatabase } = require("../mongoDB")
+const { Script } = require("../script")
+const { ObjectId } = require("mongodb")
+
+const handler = async (event) => {
+  try {
+    const requestData = JSON.parse(event.body)
+    const database = await connectToDatabase()
+    const collection = database.collection(process.env.MONGODB_COLLECTION_SCRIPTS);
+    
+    const script_id = requestData.id;
+
+    if(!script_id)
+    {
+        return {
+            statusCode: 400,
+            body: "Bad Request"
+        }
+    }
+    
+    const found_script = await collection.findOne({_id: script_id})
+    
+    if(!found_script)
+    {
+        return {
+            statusCode: 400,
+            body: "No script found with given ID."
+        }
+    }
+    
+    await collection.deleteOne({
+        _id: new ObjectId(script_id)
+    })
+
+    return {
+      statusCode: 200,
+      body: "Deleted script " + script_id + " successfully."
+    }
+  }
+  catch (error)
+  {
+    return { statusCode: 500, body: error.toString() };
+  }
+}
+
+
+module.exports = {handler}
